37fb692686ea43bf8ddcbb307952e5cc3032b56c
Yacin Bahi <yacinb@ybsis.com>
2016-02-28 14:31:27 -0800

    initial version

---
+++ lib/imuzer.rb
@@ -0,0 +1,8 @@
+require 'imuzer/version'
+require 'imuze/create_token'
+require 'imuze/create_music'
+require 'imuze/get_music'
+
+module Imuzer
+  # Your code goes here...
+end
---
+++ lib/imuzer/version.rb
@@ -0,0 +1,3 @@
+module Imuzer
+  VERSION = "0.0.1"
+end

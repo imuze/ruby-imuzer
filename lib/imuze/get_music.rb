0f87388f1473bb84ae3394278edfabc11e71a2ce
Yacin Bahi <yacinb@ybsis.com>
2016-03-14 22:30:28 -0700

    add check for existing third party tools, play or download music

--- lib/imuze/get_music.rb
+++ lib/imuze/get_music.rb
@@ -1,5 +1,5 @@
 module Imuze
-  class GetMusic < Struct.new(:token, :mp3_uri)
+  class GetMusic < Struct.new(:token, :mp3_uri, :options)
     include Methadone::CLILogging
     include Methadone::SH
     require 'uri'
@@ -11,15 +11,32 @@ module Imuze
     end
 
     def call
+      download? ? command_download : command_play
+    end
+
+    private
+
+    def command_play
+      command = %Q(curl "http:#{mp3_uri}"  -s -H "Content-Type: application/json"  -H "Authorization: #{token}" | mpg123 - 2> /dev/null)
       sh command do |stdout, stderr|
         puts 'We hope you liked it !'
       end
     end
 
-    private
+    def command_download
+      command = %Q(wget "http:#{mp3_uri}" -O #{mp3_id}.mp3)
+      sh command do |stdout, stderr|
+        puts 'We hope you liked it !'
+      end
+    end
 
-    def command
-      %Q(curl "http:#{mp3_uri}"  -s -H "Content-Type: application/json"  -H "Authorization: #{token}" | mpg123 - 2> /dev/null)
+    def download?
+      options.nil? ? false : options[:download]
+    end
+
+    def mp3_id
+      puts mp3_uri.split('/')
+      mp3_uri.split('/')[4]
     end
   end
 end
--- lib/imuzer.rb
+++ lib/imuzer.rb
@@ -4,6 +4,7 @@ require 'imuze/create_music'
 require 'imuze/get_music'
 require 'imuze/get_genres'
 require 'format_response'
+require 'find_executable'
 
 module Imuzer
   # Your code goes here...
